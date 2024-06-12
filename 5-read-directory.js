1d09c5ec4569ec463ccb8404c12f297b34fb1529
omiras <oscar.miras@gmail.com>
2024-06-12 13:53:03 +0200

    done

--- 5-read-directory.js
+++ 5-read-directory.js
@@ -1,3 +1,6 @@
 /**
- * Crea un script que muestre por consola cada uno de los archivos del directorio 'backup-files'
+ * Crea un script que muestre por consola cada uno de los archivos del directorio 'backup-files'.
+ * 
+ * REsultado esperado: https://oscarm.tinytake.com/msc/OTcyMDU5MV8yMzM1NjMzNA
+ * 
  */
--- 6-read-and-replace.js
+++ 6-read-and-replace.js
@@ -1,5 +1,11 @@
 /**
- * Crea una función que lea un fichero que remplaze todos los textos de un fichero. Se debe generar un nuevo fichero resultante de nombre "result.html"   
+ * Crea una función que lea un fichero que remplaze todos los textos de un fichero. Se debe generar un nuevo fichero resultante de nombre "result.html" .
+ * 
+ * Resultado esperado tras la primera llamada a la función: https://oscarm.tinytake.com/msc/OTcyMDYyOV8yMzM1NjM3Mg
+ * 
+ * @param {string} htmlFilePath Ruta al fichero a modificar
+ * @param {string} textToSearch Texto a buscar
+ * @param {string} textToReplace Texto a reemplazar     
  */
 
 function replaceTextInFile(htmlFilePath, textToSearch, textToReplace) {
